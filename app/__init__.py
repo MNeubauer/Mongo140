2bb38a2f468d60e4d8d1230fbf13a6c23c39417a
judahschvimer <jaschvimer@gmail.com>
2014-07-10 14:51:44 -0400

    setup

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+from app import views
+from app import filters
+
+db = MongoEngine(app)
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/config.py
@@ -0,0 +1,2 @@
+MONGODB_SETTINGS = {'DB': 'skunks'}
+SECRET_KEY = "KeepThisS3cr3t"
