68ad7f078f03de5d65347578edb80c653baef5a6
judahschvimer <jaschvimer@gmail.com>
2014-07-10 16:35:50 -0400

    added views

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import  request, redirect, render_template, url_for
+from flask.views import MethodView
+import models
+from . import app
+import json
+from bson import json_util
+import logging
+import urllib
+
+logger = logging.getLogger('views')
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello World"
+
