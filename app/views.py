aee3d49f89737d0bffd5e786c4f3a66101d6cd91
krishnakid <rahul.m.dhodapkar@gmail.com>
2014-07-11 10:28:07 -0400

    added twitter functionality, connected to @rahuldhodapkar, need to authenticate OAuth again before running a demo

--- app/views.py
+++ app/views.py
@@ -6,12 +6,37 @@ import json
 from bson import json_util
 import logging
 import urllib
+import twitter_engine as twe
+import twitter_listener as tl
 
 logger = logging.getLogger('views')
 logging.basicConfig(level=logging.DEBUG)
 
+bg = None
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    return app.config['SECRET_KEY'] 
+
+# start twitter loop running~
+@app.route('/twitter')
+def twitter():
+    print "twitter hit"
+    twt = twe.TwitterEngine()
+    bg = tl.TwitterListener(twt) 
+    print "successfully constructed"
+    bg.start()
+    print "running"
+    return 'helloworld'
+
+@app.route('/listen')
+def listen():
+    return 'test'
+
+@app.route('/successauth')
+def successauth():
+    return 'authentication was successful'
+
+
 
